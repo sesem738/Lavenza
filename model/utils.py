821b55745fb4c94e0cb4a2dafd81425d4d658824
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2022-08-05 22:24:03 +0000

    Update PE for 3 dim

--- model/utils.py
+++ model/utils.py
@@ -14,11 +14,9 @@ class PositionalEncoding(nn.Module):
         div_term = torch.exp(
             torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)
         )
-        # add positional embedding directly to the state/state+action embedding
-        # note: dim is a bit different from NLP embeddings
-        pe = torch.zeros(max_len, d_model)
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
         self.register_buffer("pe", pe)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
