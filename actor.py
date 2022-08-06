2c2cadacba72f55ea7b51bb9db1dc0c6a3203b3e
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2022-08-06 02:25:05 +0000

    feat: transformer in actor and critic

--- actor.py
+++ actor.py
@@ -1,9 +1,10 @@
+from xmlrpc.client import Boolean
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from model import *
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+from model.stt import STT
+from model.gtrxl import GTrXL
+from model.bert import BERT
 
 """
 1) Project observation into embedding space
@@ -11,18 +12,74 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 3) 
 
 """
+
+
 class Actor(nn.Module):
-	def __init__(self, state_dim, action_dim, max_action):
-		super(Actor, self).__init__()
+    def __init__(
+        self,
+        state_dim,
+        action_dim,
+        max_action,
+        history_len,
+        transformer_core: str = None,
+    ):
+        super(Actor, self).__init__()
 
-		self.l1 = nn.Linear(state_dim, 256)
-		self.l2 = nn.Linear(256, 256)
-		self.l3 = nn.Linear(256, action_dim)
-		
-		self.max_action = max_action
-		
+        self.transformer_core = transformer_core
 
-	def forward(self, state):
-		a = F.relu(self.l1(state))
-		a = F.relu(self.l2(a))
-		return self.max_action * torch.tanh(self.l3(a))
+        if self.transformer_core is None:
+            self.l1 = nn.Linear(state_dim, 256)
+        else:
+            valid_core = ["BERT", "GTrXL", "STT"]
+            if self.transformer_core not in valid_core:
+                raise ValueError("Invalid transformer core")
+
+            # Continuous embedding
+            self.embedding = nn.Linear(state_dim, 256)
+            self.l1 = nn.Linear(256, 256)
+
+            # import transformer
+            self.transformer = globals()[self.transformer_core](
+                d_model=256,
+                history_len=history_len,
+                nhead=2,
+                d_hid=256,
+                nlayers=2,
+                dropout=0.5,
+                gate_type="residue",
+                norm_first=False,
+            )
+
+            self.flatten = nn.Linear(256 * history_len, 256)
+
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, action_dim)
+
+        self.max_action = max_action
+
+    def forward(self, state):
+        x = state
+        if self.transformer_core is not None:
+            # Continuous embedding
+            x = self.embedding(x)
+            x = self.transformer(x)
+            x = self.flatten(torch.flatten(x, 1))
+        else:
+            x = x[:, 0, :]
+
+        a = F.relu(self.l1(x))
+        a = F.relu(self.l2(a))
+        return self.max_action * torch.tanh(self.l3(a))
+
+
+if __name__ == "__main__":
+
+    if torch.cuda.is_available():
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.set_default_tensor_type("torch.cuda.FloatTensor")
+
+    history_len = 4
+    a = Actor(16, 1, 1, history_len, transformer_core="STT")
+    src = torch.randn(5, history_len, 16)
+    b = a(src)
+    print(b.shape, b.device)
--- critic.py
+++ critic.py
@@ -1,39 +1,107 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from model.stt import STT
+from model.gtrxl import GTrXL
+from model.bert import BERT
+
 
 class Critic(nn.Module):
-	def __init__(self, state_dim, action_dim):
-		super(Critic, self).__init__()
+    def __init__(
+        self,
+        state_dim,
+        action_dim,
+        act_embed_size,
+        history_len,
+        transformer_core: str = None,
+    ):
+        super(Critic, self).__init__()
 
-		# Q1 architecture
-		self.l1 = nn.Linear(state_dim + action_dim, 256)
-		self.l2 = nn.Linear(256, 256)
-		self.l3 = nn.Linear(256, 1)
+        # Embedding Action in higher dimensional space
+        self.action_embedded = nn.Linear(
+            action_dim, act_embed_size
+        )  # Come back to this
 
-		# Q2 architecture
-		self.l4 = nn.Linear(state_dim + action_dim, 256)
-		self.l5 = nn.Linear(256, 256)
-		self.l6 = nn.Linear(256, 1)
+        self.transformer_core = transformer_core
+
+        if self.transformer_core is None:
+            self.l1 = nn.Linear(state_dim + act_embed_size, 256)
+            self.l4 = nn.Linear(state_dim + act_embed_size, 256)
+        else:
+            valid_core = ["BERT", "GTrXL", "STT"]
+            if self.transformer_core not in valid_core:
+                raise ValueError("Invalid transformer core")
+
+            # Continuous embedding
+            self.embedding = nn.Linear(state_dim + act_embed_size, 256)
+            self.l1 = nn.Linear(256, 256)
+            self.l4 = nn.Linear(256, 256)
+
+            # import transformer
+            self.transformer = globals()[self.transformer_core](
+                d_model=256,
+                history_len=history_len,
+                nhead=2,
+                d_hid=256,
+                nlayers=2,
+                dropout=0.5,
+                gate_type="residue",
+                norm_first=False,
+            )
+
+            self.flatten = nn.Linear(256 * history_len, 256)
+
+        # Q1 architecture
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, 1)
+
+        # Q2 architecture
+        self.l5 = nn.Linear(256, 256)
+        self.l6 = nn.Linear(256, 1)
+
+    def forward(self, state, action):
+        a = self.action_embedded(action)
+        print(a.shape)
+
+        sa = torch.cat([state, a], -1)
+        print(sa.shape)
+
+        x = sa
+        if self.transformer_core is not None:
+            # Continuous embedding
+            x = self.embedding(x)
+            x = self.transformer(x)
+            x = self.flatten(torch.flatten(x, 1))
+        else:
+            x = x[:, 0, :]
+
+        q1 = F.relu(self.l1(x))
+        q1 = F.relu(self.l2(q1))
+        q1 = self.l3(q1)
+
+        q2 = F.relu(self.l4(x))
+        q2 = F.relu(self.l5(q2))
+        q2 = self.l6(q2)
+        return q1, q2
+
+    def Q1(self, state, action):
+        sa = torch.cat([state, action], 1)
+
+        q1 = F.relu(self.l1(sa))
+        q1 = F.relu(self.l2(q1))
+        q1 = self.l3(q1)
+        return q1
 
 
-	def forward(self, state, action):
-		sa = torch.cat([state, action], 1)
+if __name__ == "__main__":
 
-		q1 = F.relu(self.l1(sa))
-		q1 = F.relu(self.l2(q1))
-		q1 = self.l3(q1)
+    if torch.cuda.is_available():
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.set_default_tensor_type("torch.cuda.FloatTensor")
 
-		q2 = F.relu(self.l4(sa))
-		q2 = F.relu(self.l5(q2))
-		q2 = self.l6(q2)
-		return q1, q2
-
-
-	def Q1(self, state, action):
-		sa = torch.cat([state, action], 1)
-
-		q1 = F.relu(self.l1(sa))
-		q1 = F.relu(self.l2(q1))
-		q1 = self.l3(q1)
-		return q1
+    history_len = 4
+    a = Critic(16, 1, 16, history_len, transformer_core="STT")
+    obs = torch.randn(5, history_len, 16)
+    act = torch.randn(5, history_len, 1)
+    q1, q2 = a(obs, act)
+    print(q1.shape, q2.device)
